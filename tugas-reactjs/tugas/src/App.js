7db33ace285dda36e443bb5401e60d864e69d24c
Ayu Lestari <ayulestarinasution848@gmail.com>
2023-03-02 22:22:22 +0700

    tugas react js

---
+++ tugas-reactjs/tugas/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div id="container"></div>
+     <h1>Hai, saya ayu lestari</h1>
+     <p> saya sudah berhasil install react</p>
+    </div>
+  );
+}
+
+export default App;
